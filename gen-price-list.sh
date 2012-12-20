01d07904f17aa36f659604c48e4acfb89184a5f5
Tom Schutter <t.schutter@comcast.net>
2012-12-20 12:09:50 -0700

    gen-price-list.sh: handle spaces in dir name

--- gen-price-list.sh
+++ gen-price-list.sh
@@ -3,9 +3,9 @@
 # Generate and display PriceListRetailTaxInc.pdf
 #
 
-SCRIPT=`readlink --canonicalize $0`
-SCRIPTDIR=`dirname ${SCRIPT}`
+SCRIPT=`readlink --canonicalize "$0"`
+SCRIPTDIR=`dirname "${SCRIPT}"`
 
 cd "${SCRIPTDIR}"
 ./gen-price-list.py --verbose
-evince PriceListRetailTaxInc.pdf &
+evince PriceListRetailTaxInc.pdf
