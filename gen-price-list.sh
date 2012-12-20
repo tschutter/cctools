86b1b9e48eb90878ac587b5eae21f38af67517e4
Tom Schutter <t.schutter@comcast.net>
2012-12-20 11:59:37 -0700

    gen-price-list.sh: initial checkin

---
+++ gen-price-list.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Generate and display PriceListRetailTaxInc.pdf
+#
+
+SCRIPT=`readlink --canonicalize $0`
+SCRIPTDIR=`dirname ${SCRIPT}`
+
+cd "${SCRIPTDIR}"
+./gen-price-list.py --verbose
+evince PriceListRetailTaxInc.pdf &
