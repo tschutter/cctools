1adc1717d02f5f4476366c519bc63ff20b344fd9
Tom Schutter <t.schutter@comcast.net>
2013-01-20 08:08:21 -0700

    cclint.py: check for duplicate SKUs

--- cclint.py
+++ cclint.py
@@ -9,8 +9,15 @@ import cctools
 import optparse
 import sys
 
-def check_string(type_name, item_name, item, key, min_len):
+def product_display_name(product):
+    """Construct a display name for a product."""
+    display_name = "%s %s" % (product["SKU"], product["Product Name"])
+    return display_name.strip()
+
+
+def check_string(type_name, item_name, item, key, min_len=0):
     """Print message if item[key] is empty or shorter than min_len."""
+    is_invalid = False
     value_len = len(item[key])
     if value_len == 0:
         print "%s '%s': Value '%s' not defined" % (
@@ -18,6 +25,7 @@ def check_string(type_name, item_name, item, key, min_len):
             item_name,
             key
         )
+        is_invalid = True
     elif value_len < min_len:
         print "%s '%s': Value '%s' == '%s' is too short" % (
             type_name,
@@ -25,10 +33,13 @@ def check_string(type_name, item_name, item, key, min_len):
             key,
             item[key]
         )
+        is_invalid = True
+    return is_invalid
 
 
 def check_value_in_set(type_name, item_name, item, key, valid_values):
     """Print message if item[key] not in valid_values."""
+    is_invalid = False
     if not item[key] in valid_values:
         print "%s '%s': Invalid '%s' == '%s' not in %s" % (
             type_name,
@@ -37,11 +48,30 @@ def check_value_in_set(type_name, item_name, item, key, valid_values):
             item[key],
             valid_values
         )
+        is_invalid = True
+    return is_invalid
+
+
+def check_skus(products):
+    """Check SKUs for uniqueness."""
+    skus = dict()
+    for product in products:
+        display_name = product_display_name(product)
+        if not check_string("Product", display_name, product, "SKU"):
+            sku = product["SKU"]
+            if sku in skus:
+                print "%s '%s': SKU already used by '%s'" % (
+                    "Product",
+                    display_name,
+                    skus[sku]
+                )
+            else:
+                skus[sku] = display_name
 
 
 def check_product(product):
     """Check product for problems."""
-    display_name = "%s %s" % (product["SKU"], product["Product Name"])
+    display_name = product_display_name(product)
 
     check_string("Product", display_name, product, "Teaser", 10)
 
@@ -130,7 +160,7 @@ def main():
     )
 
     # Check category list.
-    categories = cc_browser.get_categories()
+    #categories = cc_browser.get_categories()
     #for category in categories:
     #    check_category(category)
 
@@ -141,6 +171,7 @@ def main():
 
     # Check products list.
     products = cc_browser.get_products()
+    check_skus(products)
     for product in products:
         check_product(product)
 
