d28db4352b5c1a8edfe1cf9f13143e6e4febb821
Tom Schutter <t.schutter@comcast.net>
2013-05-18 09:37:47 -0600

    gen-price-list.sh: exclude category Earrings from output

--- gen-price-list.sh
+++ gen-price-list.sh
@@ -9,7 +9,10 @@ SCRIPTDIR=`dirname "${SCRIPT}"`
 FILENAME="PriceListRetailTaxInc.pdf"
 
 # Generate the price list.
-"${SCRIPTDIR}/gen-price-list.py" --pdf-file="${FILENAME}" --verbose
+"${SCRIPTDIR}/gen-price-list.py"\
+    --exclude-category=Earrings\
+    --pdf-file="${FILENAME}"\
+    --verbose
 
 # Display the price list if it was successfully created.
 if [ -f "${FILENAME}" ]; then
