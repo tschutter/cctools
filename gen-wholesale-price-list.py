d79c601100bb9857c00d76e9eaa79ebf334d6f0b
Tom Schutter <t.schutter@comcast.net>
2013-05-19 08:04:35 -0600

    gen-wholesale-price-list.py: add total column

--- gen-wholesale-price-list.py
+++ gen-wholesale-price-list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Generates a wholesale price list in spreadsheet form.
+Generates a wholesale order form in spreadsheet form.
 """
 
 import ConfigParser
@@ -52,22 +52,24 @@ def row_number(row):
 def add_title(worksheet):
     """Add worksheet title."""
     now = datetime.datetime.now()
-    cell_text = now.strftime("Wholesale Prices as of %Y-%m-%d")
-    style = set_cell(worksheet, 0, 0, cell_text, bold=True).style
+    style = set_cell(worksheet, 0, 0, "Wholesale Order Form", bold=True).style
     style.font.size = 20
     # merge_cells not supported by openpyxl-1.5.6 (Ubuntu 12.04)
     #worksheet.merge_cells(start_row=0, start_column=0, end_row=0, end_column=2)
     worksheet.row_dimensions[1].height = 25
 
+    cell_text = now.strftime("Date: %Y-%m-%d")
+    set_cell(worksheet, 1, 0, cell_text)
 
 def add_products(options, worksheet, row, cc_browser, products):
     """Add row for each product."""
     col_category = 0
     col_description = 1
-    col_sku = 2
-    col_price = 3
-    col_qty = 4
-    col_size = 5
+    col_price = 2
+    col_qty = 3
+    col_total = 4
+    col_sku = 5
+    col_size = 6
 
     # Add header row.
     set_cell(
@@ -78,14 +80,6 @@ def add_products(options, worksheet, row, cc_browser, products):
         bold=True
     )
     set_cell(worksheet, row, col_description, "Description", bold=True)
-    set_cell(
-        worksheet,
-        row,
-        col_sku,
-        "SKU",
-        bold=True,
-        alignment_horizontal=ALIGNMENT_HORIZONTAL_RIGHT
-    )
     set_cell(
         worksheet,
         row,
@@ -105,6 +99,22 @@ def add_products(options, worksheet, row, cc_browser, products):
     set_cell(
         worksheet,
         row,
+        col_total,
+        "Total",
+        bold=True,
+        alignment_horizontal=ALIGNMENT_HORIZONTAL_RIGHT
+    )
+    set_cell(
+        worksheet,
+        row,
+        col_sku,
+        "SKU",
+        bold=True,
+        alignment_horizontal=ALIGNMENT_HORIZONTAL_RIGHT
+    )
+    set_cell(
+        worksheet,
+        row,
         col_size,
         "Size",
         bold=True
@@ -115,6 +125,7 @@ def add_products(options, worksheet, row, cc_browser, products):
     products = sorted(products, key=cc_browser.sort_key_by_category_and_name)
 
     # Group products by category.
+    first_product_row = row
     for _, product_group in itertools.groupby(
         products,
         key=cc_browser.sort_key_by_category
@@ -129,35 +140,69 @@ def add_products(options, worksheet, row, cc_browser, products):
             )
             set_cell(worksheet, row, col_category, product["Category"])
             set_cell(worksheet, row, col_description, description)
-            set_cell(worksheet, row, col_sku, product["SKU"])
             online_price = product["Price"]
             rounded_price = math.floor(float(online_price) + 0.5)
             wholesale_price = rounded_price * options.wholesale_fraction
             style = set_cell(worksheet, row, col_price, wholesale_price).style
             style.number_format.format_code = "0.00"
+            total_formula = "=IF(%s%i=\"\", \"\", %s%i * %s%i)" % (
+                col_letter(col_qty),
+                row_number(row),
+                col_letter(col_price),
+                row_number(row),
+                col_letter(col_qty),
+                row_number(row)
+            )
+            style = set_cell(worksheet, row, col_total, total_formula).style
+            style.number_format.format_code = "#,###.00"
+            set_cell(worksheet, row, col_sku, product["SKU"])
             set_cell(worksheet, row, col_size, product["Size"])
             row += 1
+    last_product_row = row - 1
 
     # Set column widths.
     worksheet.column_dimensions[col_letter(col_category)].width = 20
     worksheet.column_dimensions[col_letter(col_description)].width = 65
-    worksheet.column_dimensions[col_letter(col_sku)].width = 6
     worksheet.column_dimensions[col_letter(col_price)].width = 5
     worksheet.column_dimensions[col_letter(col_qty)].width = 5
-    worksheet.column_dimensions[col_letter(col_size)].width = 8
+    worksheet.column_dimensions[col_letter(col_total)].width = 8
+    worksheet.column_dimensions[col_letter(col_sku)].width = 6
+    worksheet.column_dimensions[col_letter(col_size)].width = 24
+
+    # Blank row.
+    row += 1
+
+    # Total quantity.
+    total_qty_formula = "=SUM(%s%i:%s%i)" % (
+        col_letter(col_qty),
+        row_number(first_product_row),
+        col_letter(col_qty),
+        row_number(last_product_row)
+    )
+    set_cell(worksheet, row, col_qty, total_qty_formula)
+
+    # Total amount.
+    total_amount_formula = "=SUM(%s%i:%s%i)" % (
+        col_letter(col_total),
+        row_number(first_product_row),
+        col_letter(col_total),
+        row_number(last_product_row)
+    )
+    style = set_cell(worksheet, row, col_total, total_amount_formula).style
+    style.number_format.format_code = "#,##0.00"
 
 
-def add_price_list(options, cc_browser, products, worksheet):
-    """Create the Wholesale Prices worksheet."""
+def add_order_form(options, cc_browser, products, worksheet):
+    """Create the Wholesale Order Form worksheet."""
 
     # Prepare worksheet.
-    worksheet.title = "Wholesale Prices"
+    worksheet.title = "Wholesale Order Form"
 
     # Add title.
     add_title(worksheet)
 
     # Blank row.
-    row = 2
+    row = 3
 
     # Add products.
     add_products(
@@ -176,7 +221,7 @@ def generate_xlsx(options, cc_browser, products):
     workbook = openpyxl.workbook.Workbook()
 
     # Create PO-Invoice worksheet.
-    add_price_list(
+    add_order_form(
         options,
         cc_browser,
         products,
@@ -194,7 +239,7 @@ def main():
         "cctools.cfg"
     )
     now = datetime.datetime.now()
-    default_xlsx_filename = now.strftime("%Y-%m-%d-WholesalePrices.xlsx")
+    default_xlsx_filename = now.strftime("%Y-%m-%d-WholesaleOrderForm.xlsx")
 
     option_parser = optparse.OptionParser(
         usage="usage: %prog [options]\n" +
