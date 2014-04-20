ddfbcd50afafe7f1c1e88b240a1831a881ea4493
Tom Schutter <t.schutter@comcast.net>
2014-04-19 21:59:15 -0600

    gen-summary.sh: initial checkin

---
+++ gen-summary.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+usage() {
+    echo "Generate and display a summary of interesting products" >&2
+    echo "" >&2
+    echo "USAGE:" >&2
+    echo "  $0 [options]" >&2
+    echo "" >&2
+    echo "OPTIONS:" >&2
+    echo "  --dir=DIR = specify output directory" >&2
+}
+
+ARGS=""
+OUTPUT_DIR=""
+for ARG in "$@"; do
+    case ${ARG} in
+        --help)
+            usage
+            exit 1
+            ;;
+        --dir=*)
+            OUTPUT_DIR="${ARG#*=}"
+            ;;
+        *)
+            ARGS="${ARGS} ${ARG}"
+            ;;
+    esac
+done
+
+SCRIPT=`readlink --canonicalize "$0"`
+SCRIPTDIR=`dirname "${SCRIPT}"`
+
+# Determine the output filename.
+FILENAME="ProductSummary.txt"
+if [ "${OUTPUT_DIR}" ]; then
+    FILENAME="${OUTPUT_DIR}/${FILENAME}"
+fi
+rm -f ${FILENAME}
+
+FIELDS="Category,Product Name,SKU,Avail,Discd"
+
+echo "Products that are viewable online and cannot be purchased online, but are available at shows:" >> "${FILENAME}"
+"${SCRIPTDIR}/ccc" list prod --fields "${FIELDS}" --filter Avail=Y --filter Discd=Y >> "${FILENAME}" ${ARGS}
+
+echo "" >> "${FILENAME}"
+echo "Products that are not viewable online, but are available at shows:" >> "${FILENAME}"
+"${SCRIPTDIR}/ccc" list prod --fields "${FIELDS}" --filter Avail=N --filter Discd=N >> "${FILENAME}" ${ARGS}
+
+echo "" >> "${FILENAME}"
+echo "Products that are not viewable online, and are not available at shows:" >> "${FILENAME}"
+"${SCRIPTDIR}/ccc" list prod --fields "${FIELDS}" --filter Avail=N --filter Discd=Y >> "${FILENAME}" ${ARGS}
+
+# Display the output file if it was successfully created.
+if [ -f "${FILENAME}" ]; then
+    # Enable monitor mode (job control).  If not set, evince will exit
+    # when this shell exits.
+    echo "Opening ${FILENAME}"
+    set -m
+    xdg-open "${FILENAME}" &
+else
+    echo "ERROR: '${FILENAME}' not created"
+    read -p "Press [Enter] to continue..." key
+fi
