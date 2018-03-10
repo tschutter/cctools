e55c31d7a14e9b8d995f06b5a599b8313b9afd43
Tom Schutter <t.schutter@comcast.net>
2018-03-10 09:48:14 -0700

    gen-retail-plus30-line-sheet.sh -> gen-retail-line-sheet.sh: added --basename

--- gen-retail-line-sheet.sh
+++ gen-retail-line-sheet.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 
 usage() {
-    echo "Generate and display YYYY-MM-DD-RetailPlus30LineSheet.xlsx" >&2
+    echo "Generate and display YYYY-MM-DD-BASENAME.xlsx" >&2
     echo "" >&2
     echo "USAGE:" >&2
     echo "  $0 [options]" >&2
     echo "" >&2
     echo "OPTIONS:" >&2
-    echo "  --dir=DIR = specify output directory" >&2
+    echo "  --basename=NAME = filename part after date prefix" >&2
+    echo "  --dir=DIR = output directory" >&2
+    echo "  other options are passed to gen-wholesale-line-sheet.py"
+    echo "  useful options include:"
+    echo "    --price-multiplier"
+    echo "    --price-precision"
 }
 
 ARGS=""
+BASENAME="RetailLineSheet"
 OUTPUT_DIR=""
 for ARG in "$@"; do
     case ${ARG} in
         --help)
             usage
             exit 1
+            ;;
+        --basename=*)
+            BASENAME="${ARG#*=}"
             ;;
         --dir=*)
             OUTPUT_DIR="${ARG#*=}"
@@ -31,7 +40,7 @@ SCRIPT=`readlink -f "$0"`
 SCRIPTDIR=`dirname "${SCRIPT}"`
 
 # Determine the output filename.
-FILENAME="`date +%Y-%m-%d`-RetailPlus30LineSheet.xlsx"
+FILENAME="`date +%Y-%m-%d`-${BASENAME}.xlsx"
 if [ "${OUTPUT_DIR}" ]; then
     FILENAME="${OUTPUT_DIR}/${FILENAME}"
 fi
@@ -43,8 +52,7 @@ rm -f ${FILENAME}
 # SKU 30001 = All other Earrings
 "${SCRIPTDIR}/gen-wholesale-line-sheet.py"\
     --outfile="${FILENAME}"\
-    --price-multiplier=1.3\
-    --price-precision=1.0\
+    --wholesale-fraction=1\
     --exclude-sku=30001\
     --verbose\
     ${ARGS}
